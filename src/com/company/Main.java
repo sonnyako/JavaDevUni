694ab47974e29b6f7a56c91bb9e02fa164870e50
sonyako <sofiamakidon@gmail.com>
2020-09-15 00:06:03 +0300

    Task 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String myName = "Makydon Sofiia";
+        byte age = 21;
+        float height = 5.41339f;
+        double weight = 119.5;
+        char group = 'A';
+        int heightHoverla = 2061;
+        long countOfFollowers = 1503421;
+        boolean isFalse = false;
+        System.out.print("Hi! My name is " + myName + ". I'm " + age + " years old. My weight is " + weight + " pounds. "
+            + "My height is " + height + " feet." + "I'm a student of group 243" + group + "(ск)." + "This summer i " +
+            "successfully climed Mount " + "Hoverla " + heightHoverla + "m high, it was realy hard. I have " + countOfFollowers
+            + " followers on Tik-Tok. I wrote 2 " + isFalse + " facts about myself in this text.");
+
+    }
+}
